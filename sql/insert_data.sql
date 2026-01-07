11421326b1d7de2dec6852bb6204dc37005de53d
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-07 10:24:29 +0100

    add data to training table

---
+++ sql/insert_data.sql
@@ -0,0 +1,8 @@
+/*Table Training*/
+INSERT INTO f_training(t_name, t_summary, t_duration, t_is_in_person, t_is_remotely, t_price)
+VALUES
+("Java", "Java SE 8 : Suncaxe & Poo", 20, TRUE, TRUE, 55),
+("Java avancé", "Exceptions, fichiers, Jdbc, thread...", 20, TRUE, TRUE, 55),
+("Spring", "Spring Core / Mvc / Security", 20, TRUE, TRUE, 65),
+("PHP Frameworks","Symfony", 15, TRUE, FALSE, 25),
+("C#","DotNet Core",20, FALSE, TRUE, 45);
