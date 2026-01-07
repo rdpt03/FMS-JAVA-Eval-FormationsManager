387b9dbb117ed1cba2ad743391e0fd808f8916d9
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-07 10:26:37 +0100

    Add attributes

--- src/fr/fms/entities/Training.java
+++ src/fr/fms/entities/Training.java
@@ -1,5 +1,11 @@
 package fr.fms.entities;
 
 public class Training {
-
+	private int id;
+	private String name;
+	private String summary;
+	private boolean isInPerson;
+	private boolean isRemotely;
+	private double price;
+	
 }
