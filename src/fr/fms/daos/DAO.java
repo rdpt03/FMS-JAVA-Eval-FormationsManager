7a1d2c8587edf067c2e33d8154694d253a861756
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-07 11:39:54 +0100

    add generic dao

--- src/fr/fms/daos/DAO.java
+++ src/fr/fms/daos/DAO.java
@@ -1,5 +1,34 @@
 package fr.fms.daos;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public abstract class DAO {
+
+    protected Connection conn;
+
+    private final String url = "jdbc:mysql://localhost:3306/VenteFormations";
+    private final String user = "Vente123";
+    private final String password = "Vente123";
+
+    public DAO() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // driver JDBC
+            this.conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** close connection */
+    public void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
