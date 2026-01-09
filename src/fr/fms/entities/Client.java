52a8f434e21c2b76ffbf560ae2e41991062dc7f1
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-09 15:02:38 +0100

    Add Class Client and attributes

---
+++ src/fr/fms/entities/Client.java
@@ -0,0 +1,14 @@
+package fr.fms.entities;
+
+import java.util.List;
+
+public class Client {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private List<Order> listOfOrders;
+
+}
