c43496f04d40091049971eaa1d6a3d1cbe6961ea
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-09 15:04:13 +0100

    Add Class User and attributes

---
+++ src/fr/fms/entities/User.java
@@ -0,0 +1,10 @@
+package fr.fms.entities;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private List<Order> orderList;
+}
